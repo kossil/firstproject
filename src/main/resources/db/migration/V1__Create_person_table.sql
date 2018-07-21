75931e829e32eaa553d7eda364d2913b53407ced
Atomic89 <40983425+Atomic89@users.noreply.github.com>
2018-07-21 15:39:03 +0700

    Delete V1__Create_person_table.sql

--- src/main/resources/db/migration/V1__Create_person_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE TEST.PERSON (
-  ID       NUMBER(10, 0),
-  QUESTION NVARCHAR2(50),
-  CONSTRAINT PK_QUESTIONS_ID PRIMARY KEY (ID) USING INDEX TABLESPACE USERS STORAGE (INITIAL 64 K
-                                                                                    MAXEXTENTS UNLIMITED)
-)
